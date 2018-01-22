4d0037b3e909dfedf134c42340106cb7934ebe18
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-22 13:17:46 +0800

    新增、修改、刪除SQLitedDatabase資料， 只能做一次，因為沒判斷id是否重覆或沒有

--- app/src/main/java/tw/com/shiaoshia/ex2018012201/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018012201/MainActivity.java
@@ -1,5 +1,6 @@
 package tw.com.shiaoshia.ex2018012201;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -41,14 +42,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //將資料顯示出來
     public void click02(View v) {
         File dbFile = new File(getFilesDir(),"student.db");
         SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(),null,SQLiteDatabase.OPEN_READWRITE);
         Cursor c = db.rawQuery("Select * from students",null);  //設定指標位置
         c.moveToFirst();    //將指標移至第一筆資料
         Log.d("DB",c.getString(1) + "," + c.getInt(2));
-        c.moveToNext();     //將指標移至下一筆資料
-        Log.d("DB",c.getString(1) + "," + c.getInt(2));
+        while (c.moveToNext()) {
+            Log.d("DB",c.getString(1) + "," + c.getInt(2));
+        }
+        c.close();
+        db.close();
     }
 
     //各種select的用法
@@ -59,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         Cursor c = db.rawQuery(strSql,new String[] {"2"});  //Select id=2的資料
         c.moveToFirst();
         Log.d("DB",c.getString(1) + "," + c.getInt(2));
+        c.close();
+        db.close();
     }
 
     //各種select的用法
@@ -68,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
         Cursor c = db.query("students",new String[]{"_id","name","score"},null,null,null,null,null);
         c.moveToFirst();
         Log.d("DB",c.getString(1) + "," + c.getInt(2));
+        c.close();
+        db.close();
     }
 
     //各種select的用法
@@ -77,5 +86,45 @@ public class MainActivity extends AppCompatActivity {
         Cursor c = db.query("students",new String[]{"_id","name","score"},"_id=?",new String[]{"2"},null,null,null);
         c.moveToFirst();
         Log.d("DB",c.getString(1) + "," + c.getInt(2));
+        c.close();
+        db.close();
     }
+
+    //各種Insert的用法
+    public void click06(View v) {
+        File dbFile = new File(getFilesDir(),"student.db");
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(),null,SQLiteDatabase.OPEN_READWRITE);
+        db.execSQL("Insert into students(_id,name,score) values(4,'Bob',96)");
+        db.close();
+    }
+
+    //各種Insert的用法
+    public void click07(View v) {
+        File dbFile = new File(getFilesDir(),"student.db");
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(),null,SQLiteDatabase.OPEN_READWRITE);
+        ContentValues cv = new ContentValues();
+        cv.put("_id",5);
+        cv.put("name","Mary");
+        cv.put("score",95);
+        db.insert("students",null,cv);
+        db.close();
+    }
+
+    //修改資料
+    public void click08(View v) {
+        File dbFile = new File(getFilesDir(),"student.db");
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(),null,SQLiteDatabase.OPEN_READWRITE);
+        ContentValues cv = new ContentValues();
+        cv.put("score",100);
+        db.update("students",cv,"_id=?",new String[]{"2"}); //修改id=2的資料
+        db.close();
+    }
+
+    public void click09(View v) {
+        File dbFile = new File(getFilesDir(),"student.db");
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(),null,SQLiteDatabase.OPEN_READWRITE);
+        db.delete("students","_id=?",new String[]{"2"});  //刪除id=2的資料
+
+    }
+
 }
